6a6205cca649ead8fab3a421b882e6d4173566c3
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-01-18 19:01:48 +0000

    fixed linting

--- typewriter.js
+++ typewriter.js
@@ -4,15 +4,15 @@ let print = process.stdout;
 
 const animateText = (string, seconds = 1) => {
   if (string.length === 0) {
-    print.write('\n')
+    print.write('\n');
     return;
   }
   // for (const char of sentence) {
-  setTimeout( () => {
+  setTimeout(() => {
     print.write(string[0]);
-    animateText(string.slice(1), seconds)
-  }, (seconds * 1000))
+    animateText(string.slice(1), seconds);
+  }, (seconds * 1000));
   // }
 };
 
-animateText(sentence, .1)
+animateText(sentence, .1);
