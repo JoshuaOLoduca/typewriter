90b9cd95a956ae933ff2db61f6954be6a1bb4917
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-01-18 18:57:54 +0000

    logic and execution for animating string to terminal char by char

---
+++ typewriter.js
@@ -0,0 +1,18 @@
+const sentence = "hello there from lighthouse labs";
+
+let print = process.stdout;
+
+const animateText = (string, seconds = 1) => {
+  if (string.length === 0) {
+    print.write('\n')
+    return;
+  }
+  // for (const char of sentence) {
+  setTimeout( () => {
+    print.write(string[0]);
+    animateText(string.slice(1), seconds)
+  }, (seconds * 1000))
+  // }
+};
+
+animateText(sentence, .1)
